50a5c5b84f52a436a9eb791b2a5c624ce6209129
Szymon Nowicki <s.nowick@wp.pl>
2024-07-07 15:01:00 +0200

    Added init_db function

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-a = 1
-b = 2
-c = 3
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY,
+        task NEXT NOT NULL,
+        status INTEGER NOT NULL DEFAULT 0
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
